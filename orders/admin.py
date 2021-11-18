59bd992cff36fe2d800f5a64fb8f2cd5a9841451
Kasuteryon <eduardo.castellon66i@std.uni.edu.ni>
2021-11-18 14:14:31 -0600

    Added Fist Data Charge

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Categorias, Toppings, OpcionMenu, Orden, DetalleOrden 
 
 # Register your models here.
+admin.site.register(Categorias)
+admin.site.register(Toppings)
+admin.site.register(OpcionMenu)
+admin.site.register(Orden)
+admin.site.register(DetalleOrden)
--- orders/models.py
+++ orders/models.py
@@ -5,8 +5,8 @@ from django.contrib.auth.models import User
 class Categorias(models.Model):
     nombreCategoria = models.CharField(verbose_name="Nombre Categoría", max_length=25)
 
-    def __str__(self) -> str:
-        return super().__str__()
+    def __str__(self):
+        return f"{self.nombreCategoria}"
 
 class OpcionMenu(models.Model):
     nombreOpcion = models.CharField(verbose_name="Nombre Opción", max_length=25)
@@ -14,15 +14,23 @@ class OpcionMenu(models.Model):
     tamaño = models.CharField(verbose_name="Tamaño", max_length=35)
     idCategoria = models.ForeignKey(Categorias, on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.nombreOpcion} - {self.precio} - {self.tamaño} - {self.idCategoria}"
+
 class Toppings(models.Model):
     nombreTopping = models.CharField(verbose_name="Nombre del Topping", max_length=30)
     idCategoria = models.ForeignKey(Categorias, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nombreTopping} - {self.idCategoria}"
 
 class Orden(models.Model):
     total = models.FloatField(verbose_name="Monto Total Orden")
     seCompleto = models.BooleanField(verbose_name="Orden Completa")
     idUSuario = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.total} - {self.idUSuario} - {self.seCompleto}"
 
 class DetalleOrden(models.Model):
     idOrden = models.ForeignKey(Orden, on_delete=models.SET_NULL, null=True, blank=True)
@@ -30,6 +38,12 @@ class DetalleOrden(models.Model):
     cantidad = models.IntegerField(verbose_name="Cantidad")
     subtotal = models.FloatField(verbose_name="Subtotal")
 
+    def __str__(self):
+        return f"{self.idOrden} - {self.idOpcion} - {self.cantidad} - {self.subtotal}"
+
 class DetalleOrdenTopping(models.Model):
     idDetalleOrden = models.ForeignKey(DetalleOrden, on_delete=models.SET_NULL, null=True, blank=True)
     idTopping = models.ForeignKey(Toppings, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.idDetalleOrden} - {self.idTopping}"
