9203cfe3a84d2f4774aba6bcc64d6249139a3ebc
kasuteryon <eduardocastellon2002@gmail.com>
2021-12-02 06:21:56 +0000

    Fixed admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
-from .models import Categorias, CurrentOrder, DetalleOrdenTopping, Estado, Toppings, OpcionMenu, Orden, DetalleOrden 
+from .models import Categorias, CurrentOrder, DetalleOrdenTopping, Estado, Toppings, OpcionMenu, Orden, DetalleOrden
 
 admin.site.site_header = "Administrador Pizza"
 admin.site.site_title = "Administrar"
 admin.site.index_title = "Admin Pizza"
 
 # Register your models here.
+admin.site.register(Orden)
+admin.site.register(OpcionMenu)
 admin.site.register(Categorias)
 admin.site.register(Toppings)
-admin.site.register(OpcionMenu)
-admin.site.register(Orden)
 admin.site.register(DetalleOrden)
-admin.site.register(Estado)
-admin.site.register(DetalleOrdenTopping)
-admin.site.register(CurrentOrder)
+#admin.site.register(Estado)
+#admin.site.register(DetalleOrdenTopping)
+#admin.site.register(CurrentOrder)
